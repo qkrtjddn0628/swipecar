78a7fe8a7a4542b254059e50f20b5450bdbac5a6
RollingPaper <qkrtjddn0628@naver.com>
2024-08-20 09:39:48 +0900

    자동차 스크립트 1차 수정 완료

--- Swipecar/Assets/Script/CarController.cs
+++ Swipecar/Assets/Script/CarController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
@@ -7,6 +8,9 @@ public class CarController : MonoBehaviour
     private float CarMoveSpeed;
     private Vector3 a;
     private Vector3 b;
+
+    float speed = 0;
+    Vector2 startPos;
 
     // Start is called before the first frame update
     void Start()
@@ -17,33 +21,24 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool isDown = Input.GetMouseButtonDown(0);
-        bool isUp = Input.GetMouseButtonUp(0);
-        
-        // isDown = Input.mousePosition;
-        // 1. 찍고 땐 좌포 두개를 가져온다
-        // 2. 두개의 좌표 x축을 서로 뺀다
-        // 3. 양수가 나오면 +, 음수가 나오면 -
-        // 4. 그값이 양수냐 음수냐에 if문을 써서 검사를 한다.
-        // 5. 해당 값을 변수에 넣고 움직이는 값에 넣는다.
-
-        if(isDown)
+        //스와이프 길이를 구한다
+        if(Input.GetMouseButtonDown(0))
         {
-            this.a = Input.mousePosition;
-            //this.CarMoveSpeed += 0.05f;
+            // 마우스를 클릭한 좌표
+            this.startPos = Input.mousePosition;
         }
-        else if(isUp)
+        else if(Input.GetMouseButtonUp(0))
         {
-            this.b = Input.mousePosition;
-            //this.CarMoveSpeed -= 0.096f;
+            // 마우스 버튼에서 손가락을 때었을 때 좌표
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = (endPos.x = this.startPos.x);
+
+            // 스와이프 길이를 처음 속도로 변경한다
+            this.speed = swipeLength / 500.0f;
         }
 
-        Vector3 number = a - b;
 
-
-        Debug.Log($"<Color=yellow>{number}</color>");
-        this.transform.Translate(number);
-        
-
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
     }
 }
